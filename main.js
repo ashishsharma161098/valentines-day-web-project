1391e6d25e7b10f9bb801da30ddd4decf827ceca
Ashish Sharma <ashish@Ashishs-MacBook-Air.local>
2026-02-01 16:57:37 +0530

    Changes done in Main and Index File

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Start typewriter effect for the second line
         const typedTextElement = document.querySelector(".typed-text");
         setTimeout(() => {
-            typeWriterEffect(typedTextElement, "Will you be my Valentine?");
+            typeWriterEffect(typedTextElement, "Would you like to be my Valentine? 🤍");
         }, 500); // Delay to allow smooth transition
     }
 
